4708cdeeb690b97a89bed7d7aebcdca6262bc8ec
mbrowne108 <mbrowne108@gmail.com>
2021-11-15 14:04:11 -0500

    commit tree 137a871c5bb8924e8ca0ec368d5080f7ba6d9a4b

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PlantCard from "./PlantCard";
+import NewPlantForm from "./NewPlantForm";
+import Search from "./Search";
 
-function PlantList() {
+function PlantList ({ searchText }) {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data))
+  },[plants.length])
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  function handleSearch(searchText) {
+    setSearch(searchText.toLowerCase())
+  }
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+    <div>
+      <Search handleSearch={handleSearch}/>
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <ul className="cards">5
+        {plants.map(plant => {
+          {if (plant.name.toLowerCase().includes(search) || search === "") {
+            return (
+              <PlantCard 
+                id={plant.id}
+                name={plant.name}
+                image={plant.image}
+                price={plant.price}/>
+            )
+          } else {return console.log("wtf")}
+          }
+        })}
+      </ul>
+    </div>
+  )
 }
+
 
 export default PlantList;
